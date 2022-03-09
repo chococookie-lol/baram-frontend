7221e8827b27daf44a5b552370c515e3c359b839
dolphinlmg <dolphinlmg@naver.com>
2022-03-09 19:13:21 +0900

    Update Info to functional component

--- src/component/Info.jsx
+++ src/component/Info.jsx
@@ -1,59 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SummonerInfo from './SummonerInfo';
 import MatchList from './MatchList';
 import * as Api from '../model/Api';
+import { useParams } from 'react-router-dom';
 
-export default class Info extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      userdata: null,
-      matchdata: null,
-    };
-  }
+export default function Info(props) {
+  const [userData, setUserData] = useState(null);
+  const [matchIds, setMatchIds] = useState(null);
 
-  //userdata, matchdata는 각각 data와 error를 갖고있다. 각 component에서는 error 유무를 확인하고 render한다.
+  const { name } = useParams();
 
-  componentDidUpdate(prevProps) {
-    if (this.props.params !== prevProps.params) {
-      this.setState({
-        matchdata: null,
-        userdata: null,
-      });
-      this.refresh();
-    }
-  }
+  useEffect(() => {
+    setMatchIds(null);
+    setUserData(null);
+    refresh();
+  }, [name]);
 
-  componentDidMount() {
-    this.refresh();
-  }
-
-  async refresh(){
+  async function refresh() {
     let userdata;
     let matchdata;
-    try{
-      userdata = await Api.getSummonerData(this.props.params.name);
-      this.setState({ userdata: { data: userdata } });
+    try {
+      userdata = await Api.getSummonerData(name);
+      setUserData({ data: userdata });
       matchdata = await Api.getMatchesBySummoner(userdata.puuid);
-      this.setState({ matchdata: { data: matchdata } });
-    }catch(e){
-      if(!matchdata){
-        this.setState({ matchdata: { error: e } });
+      setMatchIds({ data: matchdata });
+    } catch (e) {
+      if (!matchdata) {
+        setMatchIds({ error: e });
       }
-      if(!userdata){
-        this.setState({ userdata: { error: e.message }});
+      if (!userdata) {
+        setUserData({ error: e.message })
       }
     }
   }
 
-  render() {
-    const { matchdata, userdata } = this.state;
-    return (
-      <div id="container">
-        <SummonerInfo userdata={userdata} />
-        <MatchList matchdata={matchdata} />
-      </div>
-    );
-  }
+  return (
+    <div id="container">
+      <SummonerInfo userdata={userData} />
+      <MatchList matchdata={matchIds} />
+    </div>
+  );
 }
