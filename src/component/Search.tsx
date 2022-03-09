9ebc4195fc7e587e7c88a6dba2818631d857be95
dolphinlmg <dolphinlmg@naver.com>
2022-03-09 20:39:58 +0900

    Update css

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import '../css/Search.css';
 
 export default function Search() {
   const [summonerName, setSummonerName] = useState('');
