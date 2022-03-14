339786fa72c275e18327235807320e8f9aadbab9
junheah <97426534+junhea@users.noreply.github.com>
2022-03-14 16:27:34 +0900

    Update LazyImage

--- src/component/LazyImage.tsx
+++ src/component/LazyImage.tsx
@@ -8,9 +8,8 @@ interface LazyImageProps{
 export default function LazyImage(props: LazyImageProps){
     const [loading, setLoading] = useState<boolean>(true);
     return(
-        <div className={'lazy-image-container ' + props?.className}>
-            <div className={loading?'visible ':'hidden ' + ' loading lazy-loading ' + props?.className}/>
-            <img className={loading?'hidden ':'visible ' + props?.className} style={{visibility:loading?'hidden':'visible'}} src={props.src} onLoad={()=>setLoading(false)}/>
+        <div className={'lazy-image-container loading ' + props?.className}>
+            <img className={loading?'hidden ':'visible ' + props?.className} src={props.src} onLoad={()=>setLoading(false)}/>
         </div>
     )
 }
